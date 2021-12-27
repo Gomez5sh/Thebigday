04a21a6bdba7cbcd93187b223425f042d469aac6
gomez5sh <emmanuel.gomez@vertebrasoluciones.com>
2021-12-27 07:21:59 -0500

    [ADD] add tailwind conf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import "tailwindcss/tailwind.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Peers</h1>
+      <h1 className="font-family-inherit text-lg bold text-purple-600">
+        Hello Peers
+      </h1>
     </div>
   );
 }
