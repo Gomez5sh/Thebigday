bbc724b0b27691eeb41d5763e81bad7aacbd6466
Jose David Caro Cantor <j.caro0207@gmail.com>
2021-12-30 20:43:55 -0500

    [Create post import that sends sms to all imported guests in xlsx]

--- backend/apps/guests/admin.py
+++ backend/apps/guests/admin.py
@@ -2,8 +2,22 @@ from django.contrib import admin
 from apps.guests.models import Guests
 from import_export.admin import ImportExportActionModelAdmin
 
+from django.dispatch import receiver
+from import_export.signals import post_import
+from .utils import Util
+
+
 # Register your models here.
 
 @admin.register(Guests)
 class ImportGuestsViewAdmin(ImportExportActionModelAdmin):
     pass
+
+@receiver(post_import, dispatch_uid='balabala...')
+def _post_import(model, **kwargs):
+    for guest  in model.objects.all():
+        sms_body = f"Hola {guest.first_name} {guest.second_name} has sido invitado a la boda "
+        sms_body += "David Peralta."
+        print(guest.phone_number)
+        print(sms_body)
+        Util.send_sms(sms_body, guest.phone_number)
---
+++ backend/apps/guests/utils.py
@@ -0,0 +1,18 @@
+from twilio.rest import Client 
+
+
+class Util:
+
+    def send_sms(body, to):
+
+        account_sid = 'AC692352cdfe901c27f1813f75491b4796' 
+        auth_token = '2362829a755a7699a625420ab1f32172' 
+        client = Client(account_sid, auth_token) 
+        
+        message = client.messages.create(  
+                                    from_='+16789996078',
+                                    body=body,      
+                                    to='+57'+to
+                                ) 
+        print(message.sid)
+
