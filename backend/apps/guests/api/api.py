58597ffb97c335c027ed024f9edfc7c5af51a301
Jose David Caro Cantor <j.caro0207@gmail.com>
2022-01-07 18:25:40 -0500

    [Create post to a single guest]

--- backend/apps/guests/api/api.py
+++ backend/apps/guests/api/api.py
@@ -1,4 +1,5 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from apps.guests.api.serializers import guestSerializer
@@ -24,3 +25,15 @@ class getSingleGuestAPIView(ListAPIView):
     def get_queryset(self):
         phone = self.kwargs['phone_number']
         return self.queryset.filter(phone_number = phone)
+
+class postSingleGuestAPIView(CreateAPIView):
+    ''' Add new guest to the weeding list'''
+    serializer_class = guestSerializer
+
+    def post(self, request):
+        new_guest = request.data
+        serializer_post = self.serializer_class(data= new_guest)
+        serializer_post.is_valid(raise_exception=True)
+        serializer_post.save()
+        return Response(serializer_post.data, status= status.HTTP_201_CREATED)
+
--- backend/apps/guests/api/urls.py
+++ backend/apps/guests/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from apps.guests.api.api import getGuestsAPIView, getSingleGuestAPIView
+from apps.guests.api.api import postSingleGuestAPIView
 
 urlpatterns = [
     path('guests/', getGuestsAPIView.as_view(), name='guests'),
-    path('sigle_guest/<phone_number>', getSingleGuestAPIView.as_view(), name='single_guest')
+    path('sigle_guest/<phone_number>', getSingleGuestAPIView.as_view(), name='single_guest'),
+    path('single_guest/', postSingleGuestAPIView.as_view(), name='post_guest'),
 ]
