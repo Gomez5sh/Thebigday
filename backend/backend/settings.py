99fa72c15d3c3ff63473fbbd2d9bfd13618120d9
david-develop <david.peraltar@ab-inbev.com>
2022-01-20 18:15:57 -0500

    cors activate

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.guests',
+    'corsheaders',
 
 ]
 
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -163,3 +165,4 @@ REST_FRAMEWORK = {
 }
 
 CELERY_BROKER_URL = env.str('CELERY_BROKER_URL'),
+CORS_ORIGIN_ALLOW_ALL = True
