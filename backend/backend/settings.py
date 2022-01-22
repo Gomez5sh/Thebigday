1c979a6f290a804a40bd4b7107d89fefee41d198
david-develop <david.peraltar@ab-inbev.com>
2022-01-22 17:16:04 -0500

    static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'http://127.0.0.1:8000/media/'
 
 
