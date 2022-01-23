d055c4c75916b095bd056a628cd4bb708c821e7a
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2022-01-22 23:40:39 -0500

    update url.py, now all paths has an slash

--- backend/apps/guests/api/urls.py
+++ backend/apps/guests/api/urls.py
@@ -9,13 +9,13 @@ from apps.guests.api.api import getFamilyGroupAPIView, FormConfirmationAPIView
 
 urlpatterns = [
     path('guests/', getGuestsAPIView.as_view(), name='guests'),
-    path('sigle_guest/<phone_number>', getSingleGuestAPIView.as_view(), name='single_guest'),
+    path('sigle_guest/<phone_number>/', getSingleGuestAPIView.as_view(), name='single_guest'),
     path('single_guest/', postSingleGuestAPIView.as_view(), name='post_guest'),
     path('send_invitations/', postGuestsSENDinvitationAPIView.as_view(), name='send_invitations'),
     path('songs/', getSongsAPIView.as_view(), name='songs'),
-    path('gallery_images/<gallery_name>', getPicturesGalleryAPIView.as_view(), name='gallery'),
+    path('gallery_images/<gallery_name>/', getPicturesGalleryAPIView.as_view(), name='gallery'),
     path('donation/message/', CommentDonationsAPIView.as_view(), name='donation_message'),
-    path('singleImage', getPostSingleImagesAPIView.as_view(), name='singleImage'),
-    path('family_group/<phone_number>', getFamilyGroupAPIView.as_view(), name='family_group'),
-    path('form_confirmation', FormConfirmationAPIView.as_view(), name='form_confirmation'),
+    path('singleImage/', getPostSingleImagesAPIView.as_view(), name='singleImage'),
+    path('family_group/<phone_number>/', getFamilyGroupAPIView.as_view(), name='family_group'),
+    path('form_confirmation/', FormConfirmationAPIView.as_view(), name='form_confirmation'),
 ]
